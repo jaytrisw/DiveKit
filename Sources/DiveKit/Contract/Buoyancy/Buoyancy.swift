ef14f896d7d46335dbfbc727281120653294385d
Joshua T. Wood <joshuatw@gmail.com>
2024-04-08 18:00:11 +0200

    clean up buoyancy init

--- Sources/DiveKit/Contract/Buoyancy/Buoyancy.swift
+++ Sources/DiveKit/Contract/Buoyancy/Buoyancy.swift
@@ -4,18 +4,6 @@ public enum Buoyancy {
     case positive(_ buoyantForce: Double)
     case negative(_ buoyantForce: Double)
     case neutral
-
-    package init(_ value: Double) {
-        if value == 0 {
-            self = .neutral
-            return
-        }
-        if value < 0 {
-            self = .negative(abs(value))
-            return
-        }
-        self = .positive(value)
-    }
 }
 
 extension Buoyancy: Equatable {}
