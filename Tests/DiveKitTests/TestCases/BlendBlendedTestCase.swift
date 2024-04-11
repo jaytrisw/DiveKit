4eae15bb614811c83d9862ee69d5867fec3d4e76
Joshua T. Wood <joshuatw@gmail.com>
2024-04-11 18:44:54 +0200

    add tests for Blended initialization when State is Blended

--- Tests/DiveKitTests/TestCases/BlendBlendedTestCase.swift
+++ Tests/DiveKitTests/TestCases/BlendBlendedTestCase.swift
@@ -32,4 +32,34 @@ final class BlendBlendedTestCase: SystemUnderTestCase<Blend<Blended>> {
         XCTAssertEqual(result.fractionalPressure, 0)
         XCTAssertEqual(result.gas, gas)
     }
+
+    func testInitializationWithParameterPacks() {
+        // Given
+        let oxygen = Oxygen()
+        let oxygenFraction = 1.0
+
+        // When
+        sut = .init(.init(oxygen, fractionalPressure: oxygenFraction))
+
+        // Then
+        XCTAssertEqual(sut.partialPressure(of: oxygen).fractionalPressure, oxygenFraction)
+        XCTAssertEqual(sut.storage.count, 1)
+    }
+
+    func testInitializeWithResultBuilder() throws {
+        // Given
+        let oxygen = PartialPressure(.oxygen, fractionalPressure: 0.40)
+        let nitrogen = PartialPressure(.nitrogen, fractionalPressure: 0.60)
+
+        // When
+        sut = .init {
+            oxygen
+
+            nitrogen
+        }
+
+        // Then
+        XCTAssertEqual(sut.totalPressure, 1.0)
+        XCTAssertEqual(sut.storage.count, 2)
+    }
 }
