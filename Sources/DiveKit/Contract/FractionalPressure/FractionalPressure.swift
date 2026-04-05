337a97ddeb35fd29c1be8843b013fbfbac6f96cb
Joshua T. Wood <joshuatw@gmail.com>
2026-04-05 10:53:46 +0200

    update filenames

---
+++ Sources/DiveKit/Contract/FractionalPressure/FractionalPressure.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+public struct FractionalPressure<Gas: GasRepresentable>: Sendable {
+    public let gas: Gas
+    public let fractionalPressure: Double
+
+    @_spi(unsafe)
+    public init(_ gas: Gas, fractionalPressure: Double) {
+        self.gas = gas
+        self.fractionalPressure = fractionalPressure
+    }
+
+    public init(of gas: Gas, fractionalPressure: Double) throws(DiveKit.Error) {
+        let callSite: CallSite = .init(object: .init(describing: Self.self), function: #function)
+        try fractionalPressure.validate(using: .greaterThanOrEqual(to: .zero)) {
+            .negative(.fractionalPressure($0), callSite)
+        }
+        try fractionalPressure.validate(using: .lessThanOrEqual(to: .one)) {
+            .range(.upperBound($0, .one), .init(object: .init(describing: Self.self), function: #function))
+        }
+
+        self.gas = gas
+        self.fractionalPressure = fractionalPressure
+    }
+}
+
+extension FractionalPressure: Equatable {}
+extension FractionalPressure: Hashable {}
+extension FractionalPressure: ResultRepresentable {}
--- Sources/DiveKit/Contract/PartialPressure/PartialPressure.swift
+++ Sources/DiveKit/Contract/PartialPressure/PartialPressure.swift
@@ -1,29 +1,13 @@
 import Foundation
 
-public struct FractionalPressure<Gas: GasRepresentable>: Sendable {
-    public let gas: Gas
-    public let fractionalPressure: Double
+public struct PartialPressure<Gas: GasRepresentable>: Sendable {
+    public let value: Double
+}
 
-    @_spi(unsafe)
-    public init(_ gas: Gas, fractionalPressure: Double) {
-        self.gas = gas
-        self.fractionalPressure = fractionalPressure
-    }
-
-    public init(of gas: Gas, fractionalPressure: Double) throws(DiveKit.Error) {
-        let callSite: CallSite = .init(object: .init(describing: Self.self), function: #function)
-        try fractionalPressure.validate(using: .greaterThanOrEqual(to: .zero)) {
-            .negative(.fractionalPressure($0), callSite)
-        }
-        try fractionalPressure.validate(using: .lessThanOrEqual(to: .one)) {
-            .range(.upperBound($0, .one), .init(object: .init(describing: Self.self), function: #function))
-        }
-
-        self.gas = gas
-        self.fractionalPressure = fractionalPressure
+extension PartialPressure: DecimalRepresentable {
+    public init(_ value: Double) {
+        self.value = value
     }
 }
 
-extension FractionalPressure: Equatable {}
-extension FractionalPressure: Hashable {}
-extension FractionalPressure: ResultRepresentable {}
+extension PartialPressure: ResultRepresentable {}
