11e669be153a00d1fa6bd01903800f5303a1ad5b
Joshua T. Wood <joshuatw@gmail.com>
2026-04-19 10:34:09 +0200

    remove unused expect unwrapped

--- Tests/DiveKitTests/Utility/ExpectUnwrapped.swift
+++
@@ -1,7 +0,0 @@
-import Testing
-
-func expectUnwrapped<T>(
-    _ expression: @autoclosure () throws -> T?,
-    handler: (T) throws -> Void) throws {
-        try handler(try #require(try expression()))
-    }
