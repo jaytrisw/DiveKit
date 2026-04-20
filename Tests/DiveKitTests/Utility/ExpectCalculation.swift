384d6aa6d91077138b13c2d1f1506737332b330f
Joshua T. Wood <joshuatw@gmail.com>
2026-04-20 21:07:45 +0200

    migrate atmospheres absolute to parametrized tests

--- Tests/DiveKitTests/Utility/ExpectCalculation.swift
+++ Tests/DiveKitTests/Utility/ExpectCalculation.swift
@@ -8,3 +8,15 @@ package func expectCalculation<Result: ResultRepresentable>(
         let calculation = try calculation()
         try handler(calculation.result, calculation.configuration)
     }
+
+package func given<S, Result>(
+    _ given: () throws -> S,
+    when: (_ sut: S) throws -> Result,
+    then: (_ sut: S, _ result: Result) throws -> Void) async rethrows {
+        try await confirmation { confirmation in
+            let sut = try given()
+            let result = try when(sut)
+            try then(sut, result)
+            confirmation()
+        }
+    }
---
+++ Tests/DiveKitTests/Utility/Tags.swift
@@ -0,0 +1,10 @@
+import Testing
+import DiveKit
+
+extension Tag {
+    @Tag static var atmospheresAbsolute: Self
+    @Tag static var freshWater: Self
+    @Tag static var imperial: Self
+    @Tag static var metric: Self
+    @Tag static var saltWater: Self
+}
